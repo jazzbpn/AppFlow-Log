65e8990be7750b3b4637a457a9967ab6580e286e
bipin <jazzbpn@gmail.com>
2018-09-06 15:46:59 +0545

    First Commit

---
+++ AppFlow/AppFlow.h
@@ -0,0 +1,19 @@
+//
+//  AppFlow.h
+//  AppFlow
+//
+//  Created by Bipin on 9/6/18.
+//  Copyright © 2018 Tootle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AppFlow.
+FOUNDATION_EXPORT double AppFlowVersionNumber;
+
+//! Project version string for AppFlow.
+FOUNDATION_EXPORT const unsigned char AppFlowVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AppFlow/PublicHeader.h>
+
+
